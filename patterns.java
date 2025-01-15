08033a09bf19b634bf49c10e2d78c6c7e12a7f2f
gautham <desertncamel@gmail.com>
2025-01-15 17:27:31 +0530

    pattern one

--- patterns.java
+++ patterns.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class patterns {
+
+    static void printer(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("HI");
+        var sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printer(n);
+        sc.close();
     }
 }
