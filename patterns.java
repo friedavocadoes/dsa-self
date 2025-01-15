7d464ce22e17e94436324c1da78ba28435698d14
gautham <desertncamel@gmail.com>
2025-01-15 19:37:21 +0530

    array

--- patterns.java
+++ patterns.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class patterns {
 
     static void printer(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
